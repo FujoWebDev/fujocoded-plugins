e09a2b71a4d5e4b28751f6118e1b3714f331bb38
Essential Randomness <essentialrandomn3ss@gmail.com>
2025-09-11 17:21:27 -0700

    target images with alt text

--- astro-alt-text-toolkit/src/index.ts
+++ astro-alt-text-toolkit/src/index.ts
@@ -1,1 +1,10 @@
-document.body.style.backgroundColor = "red";
+/**
+ * Adds a alt text badge to all images on the site.
+ */
+
+const images = Array.from(
+  document.querySelectorAll<HTMLImageElement>("img[alt]")
+);
+for (const image of images) {
+  image.style.transform = "rotate(180deg)";
+}
