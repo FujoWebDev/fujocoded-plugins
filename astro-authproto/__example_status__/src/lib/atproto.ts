e72f749e6b010984abcd680ab7851b7f2f8c02a6
Essential Randomness <essentialrandomn3ss@gmail.com>
2025-10-09 20:30:46 -0700

    go a bit overboard

--- astro-authproto/__example_status__/src/lib/atproto.ts
+++ astro-authproto/__example_status__/src/lib/atproto.ts
@@ -2,21 +2,31 @@ import type { APIContext } from "astro";
 import { AtpBaseClient } from "@atproto/api";
 import { DidResolver } from "@atproto/identity"; // this is already included with @fujocoded/authproto!
 
-export async function getAgent(locals: APIContext["locals"]) {
-  const loggedInUser = locals.loggedInUser;
+export async function getLoggedInAgent(loggedInUser: NonNullable<App.Locals["loggedInUser"]>) {
   try {
-    const agent = new AtpBaseClient(loggedInUser?.fetchHandler!);
+    const agent = new AtpBaseClient(loggedInUser.fetchHandler);
     return agent;
   } catch (error) {
-    return;
+    return null;
   }
 }
 
+export async function getPublicAgent() {
+  try {
+    const agent = new AtpBaseClient("https://public.api.bsky.app/");
+    return agent;
+  } catch (error) {
+    return null;
+  }
+}
+
+// TODO: explain this better
+// TODO: Do we really want this here? are we using it anywhere?
 // this is good if you want to resolve a did into a user-friendly handle
-const RESOLVER = new DidResolver({});
 export async function didToHandle(did: string) {
   try {
-    const atProtoData = await RESOLVER.resolveAtprotoData(did);
+    const didResolver = new DidResolver({});
+    const atProtoData = await didResolver.resolveAtprotoData(did);
     return atProtoData.handle;
   } catch (error) {
     return "Invalid handle";
