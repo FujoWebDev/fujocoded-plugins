078099a8d9498b12cad8da012016e57267e0136e
Essential Randomness <essentialrandomn3ss@gmail.com>
2026-01-15 17:11:51 -0800

    change build to tsdown

---
+++ astro-ao3-loader/tsdown.config.ts
@@ -0,0 +1,12 @@
+import { defineConfig } from "tsdown";
+
+export default defineConfig([
+  {
+    name: "astro-ao3-loader",
+    dts: true,
+    clean: true,
+    unbundle: true,
+    entry: ["src/index.ts"],
+    external: [/^astro:?/],
+  },
+]);
