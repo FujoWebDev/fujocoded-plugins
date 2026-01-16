0ce33475720afc37edaefa3104d9baa31fd5e4b3
Essential Randomness <essentialrandomn3ss@gmail.com>
2026-01-15 21:10:32 -0800

    Changesets for ao3 loader

--- astro-ao3-loader/__examples__/astro-5/src/content/config.ts
+++ astro-ao3-loader/__examples__/astro-5/src/content/config.ts
@@ -1,6 +1,7 @@
 import { defineCollection } from "astro:content";
-import { worksLoader } from "@fujocoded/astro-ao3-loader";
+import { worksLoader, seriesLoader } from "@fujocoded/astro-ao3-loader";
 
 export const collections = {
   fanfictions: defineCollection({ loader: worksLoader }),
+  series: defineCollection({ loader: seriesLoader }),
 };
--- astro-ao3-loader/src/index.ts
+++ astro-ao3-loader/src/index.ts
@@ -1,4 +1,4 @@
-import { getWork, setFetcher } from "@fujocoded/ao3.js";
+import { getWork, getSeries, setFetcher } from "@fujocoded/ao3.js";
 import { readFileSync } from "node:fs";
 import { WorkSummarySchema } from "./schemas.ts";
 import { parse } from "yaml";
@@ -20,6 +20,41 @@ export const worksLoader: Loader = {
       const fetchedWorks = await Promise.allSettled(
         nextBatch.value?.map((workId) =>
           getWork({ workId: workId.toString() })
+        ) ?? []
+      );
+      fetchedWorks.forEach((response) => {
+        if (response.status == "rejected") {
+          return;
+        }
+        logger.info(`Setting data for fic ${response.value.id}`);
+        // @ts-expect-error TODO: apparently interfaces and types treat
+        // index signatures differently
+        store.set({ id: response.value.id, data: response.value });
+      });
+      if (nextBatch.done) {
+        break;
+      }
+    }
+  },
+  schema: WorkSummarySchema,
+};
+
+
+export const seriesLoader: Loader = {
+  name: "ao3-series-loader",
+  load: async ({ store, logger }: LoaderContext) => {
+    setFetcher(getFetcher(logger));
+    const file = readFileSync("./src/content/ao3/series.yaml", {
+      encoding: "utf-8",
+    });
+    const seriesId = (parse(file) as any[]).map((workId) => workId.toString());
+    const ficGroupsFetcher = getNextFicGroupFetcher(seriesId, logger)();
+    while (true) {
+      const nextBatch = ficGroupsFetcher.next();
+      logger.info(`Loading fics ${nextBatch.value?.join(", ")}`);
+      const fetchedWorks = await Promise.allSettled(
+        nextBatch.value?.map((workId) =>
+          getSeries({ seriesId: workId.toString() })
         ) ?? []
       );
       fetchedWorks.forEach((response) => {
